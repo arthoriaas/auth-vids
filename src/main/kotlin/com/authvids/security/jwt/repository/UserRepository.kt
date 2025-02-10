0dec14897509aebd230b4ee259e3ebbf578fb03e
Arthur Rosa Pereira <arthur.rosa.pereira@sap.com>
2025-02-09 23:44:40 -0300

    Fix

--- src/main/kotlin/com/authvids/security/jwt/repository/UserRepository.kt
+++ src/main/kotlin/com/authvids/security/jwt/repository/UserRepository.kt
@@ -13,19 +13,19 @@ class UserRepository(
     private val users = mutableSetOf(
       User(
         id = UUID.randomUUID(),
-        name = "email-1@gmail.com",
+        name = "email1@gmail.com",
         password = encoder.encode("pass1"),
         role = Role.USER,
       ),
       User(
         id = UUID.randomUUID(),
-        name = "email-2@gmail.com",
+        name = "email2@gmail.com",
         password = encoder.encode("pass2"),
         role = Role.ADMIN,
       ),
       User(
         id = UUID.randomUUID(),
-        name = "email-3@gmail.com",
+        name = "email3@gmail.com",
         password = encoder.encode("pass3"),
         role = Role.USER,
       )
